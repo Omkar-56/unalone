75d55b629fa0a48a2afb71c179969b4665df4ae2
Omkar-56 <omkarpansare5566@gmail.com>
2026-02-12 20:05:19 +0530

    add login route and jwt middleware

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import validator from "validator";
-import pool from "../db/index.js"
+import jwt from "jsonwebtoken";
+import pool from "../db/index.js";
 import otpStore from "../utils/otpStore.js";
 import { generateOtp } from "../utils/generateOtp.js";
 import { registerSchema } from "../validators/authSchema.js";
 import { sendOtpEmail } from "../utils/mailer.js";
-
 
 const router = express.Router();
 
@@ -104,6 +104,68 @@ router.post("/verify-otp", (req, res) => {
 
 
   res.json({ message: "Email verified" });
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // -------- Validation --------
+    if (!email || !password) {
+      return res.status(400).json({
+        error: "Email and password are required",
+      });
+    }
+
+    // -------- Find user --------
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(400).json({
+        error: "Invalid credentials",
+      });
+    }
+
+    const user = result.rows[0];
+
+    // -------- Check password --------
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password_hash
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        error: "Invalid credentials",
+      });
+    }
+
+    // -------- Check verification --------
+    if (user.verification_status !== "email_verified") {
+      return res.status(403).json({
+        error: "Email not verified",
+      });
+    }
+
+    // -------- Create JWT --------
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 
