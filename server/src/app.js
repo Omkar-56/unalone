082b52b2d903778ff10a5c7e6754285b0197825d
Omkar-56 <omkarpansare5566@gmail.com>
2026-06-12 21:01:09 +0530

    fix cors config

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://unalone-flax.vercel.app"],
   credentials: true
 }
 
