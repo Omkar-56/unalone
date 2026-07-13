c09897ff191c2b2d54dd829c4b6609fb52f06262
Omkar-56 <omkarpansare5566@gmail.com>
2026-07-13 20:46:25 +0530

    change smtp port from 465 to 587

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -4,7 +4,9 @@ import dns from "dns";
 dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
