b3c4040cdd4095e5d8efa87f180456a4bca2a96b
Omkar-56 <omkarpansare5566@gmail.com>
2026-07-15 21:04:36 +0530

    use brevo email api

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -1,28 +1,9 @@
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_SERVER,
-  port: Number(process.env.SMTP_PORT),
-  secure: Number(process.env.SMTP_PORT) === 465,
-  auth: {
-    user: process.env.SMTP_LOGIN,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
-
-transporter.verify((err) => {
-  if (err) {
-    console.error("SMTP verification failed:", err);
-  } else {
-    console.log("SMTP server ready");
-  }
-});
-
+import axios from "axios";
 export async function sendOtpEmail(email, otp) {
   try {
-    await transporter.sendMail({
-      from: `Unalone <${process.env.EMAIL_USER}>`,
-      to: email,
+    await axios.post("https://api.brevo.com/v3/smtp/email", {
+      sender: { name: "Unalone", email: process.env.EMAIL_USER },
+      to: [{ email }],
       subject: "Your verification code",
       text: `Your OTP is ${otp}`,
     });
